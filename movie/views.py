645449df27b0c6307566f730584d6d52afc87ba1
Samuel Moncada <smoncadam@eafit.edu.co>
2026-02-05 17:47:18 -0500

    Busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Samuel Moncada'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
-    return HttpResponse("<h1>About Movie Reviews</h1>")
+    return render(request, 'about.html')
